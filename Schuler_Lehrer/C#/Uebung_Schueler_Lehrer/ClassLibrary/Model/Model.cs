b19006bf1f8e3c897ded5500c661dd9f387431e4
Florian Friedl <20150066@htblaperg.onmicrosoft.com>
2020-05-20 16:11:41 +0200

    First Push

---
+++ Schuler_Lehrer/C#/Uebung_Schueler_Lehrer/ClassLibrary/Model/Model.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClassLibrary.Model
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public decimal AverageGrade { get; set; }
+
+        public int ClassId { get; set; }
+        public Class Class { get; set; }
+    }
+
+    public class Class
+    {
+        [Key]
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        
+        public ICollection<Student> Students { get; set; }
+
+        public ICollection<ClassTeacher> ClassTeachers { get; set; }
+
+    }
+
+    public class Teacher
+    {
+        [Key]
+        public int TeacherId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+
+        public ICollection<ClassTeacher> ClassTeachers { get; set; }
+    }
+
+    public class ClassTeacher
+    {
+        public int ClassId { get; set; }
+        public Class Class { get; set; }
+
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
---
+++ Schuler_Lehrer/C#/Uebung_Schueler_Lehrer/Uebung_Schueler_Lehrer/SchoolDbContext.cs
@@ -0,0 +1,44 @@
+﻿using ClassLibrary.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uebung_Schueler_Lehrer
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options) { }
+
+
+        public DbSet<Student> Students{ get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ClassTeacher>()
+                .HasKey(c => new { c.ClassId, c.TeacherId });
+
+            modelBuilder.Entity<ClassTeacher>()
+                .HasOne(ct => ct.Class)
+                .WithMany(c => c.ClassTeachers)
+                .HasForeignKey(ct => ct.TeacherId);
+
+            modelBuilder.Entity<ClassTeacher>()
+                .HasOne(ct => ct.Teacher)
+                .WithMany(t => t.ClassTeachers)
+                .HasForeignKey(ct => ct.ClassId);
+
+            modelBuilder.Entity<Student>()
+                .HasOne(s => s.Class)
+                .WithMany(c => c.Students)
+                .HasForeignKey(s => s.ClassId);
+
+
+        }
+    }
+}
